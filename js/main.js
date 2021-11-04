ee6aa716a53b7177beadda5e7e1a70c82dc75cc0
Nuccia955 <elenamunaro1995@gmail.com>
2021-11-04 14:02:19 +0100

    sketch task

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
-console.log('test JS');
+/* TASK:
+1. Creiamo il nostro array di oggetti che rappresentano ciascun membro del team. Ogni membro (oggetto) dovrà avere le informazioni (chiavi) necessarie per stampare la relativa card: Nome, Ruolo e Foto.
+
+
+2.stampiamo tutte le card del nostro team.
+  -refs nel DOM
+  -invoca funzione per generare le card
+  -inserire nell'html
+
+
+3. Utilizziamo poi gli input presenti nella pagina per permettere all’utente di aggiungere nuovi membri del team.
+  -refs nel DOM per selezionare valori inseriti nella form
+  -invoca funzione per generare nuovo membro
+  -inserisci oggetto nell'array 
+*/
+
+
+
+
+/***********
+ * FUNZIONI
+ * *********/
+//1. funzione per generare le card
+
+
+//2. funzione per generare nuovo oggetto (nuovo membro del team)
